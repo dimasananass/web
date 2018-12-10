55d0594ef96683fdd9ee02b3a150055cb6e87e48
Dima Batih <32327111+dimasananass@users.noreply.github.com>
2018-12-11 00:13:22 +0200

    Add files via upload

---
+++ 3/jqueri/js2.js
@@ -0,0 +1,12 @@
+alert($("#elem").html());
+
+ $( document ).ready(function() {
+	 
+        let user = $('#list-header');
+    user.css('font-weight', 'bold');
+    user.append('2');
+
+var zamina = $("#name").html().replace('Peter','Dmutro');
+      $("#name").html(zamina);
+	 
+    });
